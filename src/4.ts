06e5cb180e5bdf98fa6837ac621bec0fc7d42a15
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-11-08 14:53:51 +0200

    finish all tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,36 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(protected key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  private tenants: Person[];
+  protected door: boolean;
+  constructor(protected key: Key) {}
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    this.door = this.key.getSignature() === key.getSignature();
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +39,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
